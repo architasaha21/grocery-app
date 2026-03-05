e4a7408cbc68fd6e88a14c9db5c049c3dd561d4a
architasaha21 <archita.saha2106@gmail.com>
2026-03-05 16:47:07 +0530

    New user registration

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,47 @@
+import UserModel from "../models/user.model.js"
+import bcryptjs from "bcryptjs" 
+
+export async function registerUserController(req,res){
+    try{
+        const { name, email, password } = req.body
+
+        if(!name || !email || !password){
+            return res.status(400).json({
+                message : "All fields are required",
+                error : true,
+                success : false
+            })
+        }
+
+        const user = await UserModel.findOne({ email})
+
+        // checking if email was already registered
+        if(user){
+            return res.json ({
+                message : "User already exists",
+                error : true,
+                success : false
+            })
+        }
+
+        //encrypting password
+        const salt = await bcryptjs.genSalt(10)
+        const hashedPassword = await bcryptjs.hash(password,salt)
+
+        const payload = {
+            name,
+            email,
+            password : hashedPassword   
+        }
+
+        const newUser = new UserModel(payload)
+        const save = await newUser.save()
+    }
+    catch(error){
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+}
